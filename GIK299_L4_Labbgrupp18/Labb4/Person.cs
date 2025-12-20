722c015d47f1abef62004ef24e4bba40c2274230
pixl00 <dennis.loov@gmail.com>
2025-12-20 19:47:51 +0100

    Moved person to its own file

---
+++ GIK299_L4_Labbgrupp18/Labb4/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb4
+{
+    enum Gender
+    {
+        woman,
+        man,
+        non_binary,
+        other
+    }
+
+    internal class Person
+    {
+        Gender Gender;
+        Hair Hair;
+        DateTime BirthDay;
+        Color EyeColor;
+
+        public Person( Gender gender, Hair hair, DateTime birthDate, Color eyeColor )
+        {
+            Gender = gender;
+            Hair = hair;
+            BirthDay = birthDate;
+            EyeColor = eyeColor;
+        }
+
+        public string GetData()
+        {
+            return
+                Gender.ToString() + "," +
+                Hair.HairLength.ToString() + "," +
+                Hair.HairColor.ToString() + "," +
+                BirthDay.ToString() + "," +
+                EyeColor.ToString();
+        }
+    }
+
+    struct Hair
+    {
+        public float HairLength;
+        public Color HairColor;
+
+        public Hair( float hairLength, Color hairColor )
+        {
+            HairLength = hairLength;
+            HairColor = hairColor;
+        }
+    }
+}
--- GIK299_L4_Labbgrupp18/Labb4/Program.cs
+++ GIK299_L4_Labbgrupp18/Labb4/Program.cs
@@ -2,58 +2,12 @@
 
 namespace Labb4
 {
-    enum Gender
-    {
-        woman,
-        man,
-        non_binary,
-        other
-    }
-
-    struct Hair
-    {
-        public float HairLength;
-        public Color HairColor;
-
-        public Hair( float hairLength, Color hairColor )
-        {
-            HairLength = hairLength;
-            HairColor = hairColor;
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args)
         {
             Person person = new Person( Gender.man, new Hair( 10f, Color.Brown ), new DateTime(2003, 10, 27), Color.Blue );
             Console.WriteLine(person.GetData());
-        }
-    }
-
-    class Person
-    {
-        Gender Gender;
-        Hair Hair;
-        DateTime BirthDay;
-        Color EyeColor;
-
-        public Person( Gender gender, Hair hair, DateTime birthDate, Color eyeColor )
-        {
-            Gender = gender;
-            Hair = hair;
-            BirthDay = birthDate;
-            EyeColor = eyeColor;
-        }
-
-        public string GetData()
-        {
-            return 
-                Gender.ToString() + "," + 
-                Hair.HairLength.ToString() + "," +
-                Hair.HairColor.ToString() + "," +
-                BirthDay.ToString() + "," +
-                EyeColor.ToString();
         }
     }
 }
